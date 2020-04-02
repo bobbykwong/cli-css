8992df002253ede96a3a543cb3fa3acb66b09e8c
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-04-02 22:29:55 +0800

    script able to convert all inputs

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const rgb2HEXHSL = require('./rgb-2-hex');
 
 const callFunction = (source, arg1, arg2, arg3) => {
     if (source === "hex") {
-        return hex2RGBHSL.hexToRgbHsl(process.argv[3]);
+        return hex2RGBHSL.hexToRgbHsl(arg1);
     }
     else if (source === "rgb"){
-        return rgb2HEXHSL.rgbToHexHsl(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5]));
+        return rgb2HEXHSL.rgbToHexHsl(parseInt(arg1), parseInt(arg2), parseInt(arg3));
     }
 }
 
-callFunction(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
+
+// this special console will create colored output
+// console.log(`\x1b[38;2;${process.argv[3]};${process.argv[4]};${process.argv[5]}m%s\x1b[0m`, callFunction(process.argv[2], process.argv[3], process.argv[4], process.argv[5]));
+
+for(let i=3; i<process.argv.length-3; i++){
+    callFunction(process.argv[2] ,process.argv[i]);
+    console.log(process.argv[i]);
+}
