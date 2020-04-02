1f2bab0207bac3434ab2e0d0d65d1fc604b5d8ca
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-04-02 19:30:03 +0800

    able to convert to hsl as well

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const hex2RGB = require('./hex-2-rgb.js');
-const rgb2HEX = require('./rgb-2-hex');
+const hex2RGBHSL = require('./hex-2-rgb.js');
+const rgb2HEXHSL = require('./rgb-2-hex');
 
 // console.log(process.argv[3])
 
 const callFunction = (source, arg1, arg2, arg3) => {
     if (source === "hex") {
-        console.log(hex2RGB.hexToRgb(process.argv[3]));
+        return hex2RGBHSL.hexToRgbHsl(process.argv[3]);
     }
     else if (source === "rgb"){
-        console.log(rgb2HEX.rgbToHex(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5])));
+        return rgb2HEXHSL.rgbToHexHsl(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5]));
     }
 }
 
--- rgb-2-hex.js
+++ rgb-2-hex.js
@@ -3,8 +3,34 @@ function componentToHex(c) {
   return hex.length == 1 ? "0" + hex : hex;
 }
 
-module.exports.rgbToHex = function(r, g, b) {
+const rgbToHex = (r, g, b) => {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 // alert(rgbToHex(0, 51, 255)); // #0033ff
+
+const rgbToHsl = (r, g, b) => {
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
+
+module.exports.rgbToHexHsl = function(r, g, b){
+    console.log(rgbToHex(r, g, b));
+    console.log(rgbToHsl(r, g, b));
+}
